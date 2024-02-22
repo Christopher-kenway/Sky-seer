82749e962a2fcab8a220755d77c746f5fbc66c42
christopher-kenway <maero1eguwe@gmail.com>
2024-02-22 09:57:50 +0100

    new changes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const apiKey = "68b9b9342555e5ba07176f3590fe84af";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid= ${apiKey}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=${apiKey}`;
+console.log(apiUrl);
 
-const city = document.getElementById("city").value;
+const city = document.getElementById("locationInput").value;
 
 
 async function checkWeather(){
-    
+    const response = await fetch(apiUrl);
+    let data = await response.json();
+    console.log(data);
 }
+
+checkWeather();
